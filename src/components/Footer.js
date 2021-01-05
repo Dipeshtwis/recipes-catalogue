dfd9fde55879841f1a70127cbdcb3dffa4c54701
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 00:43:58 +0530

    Add footer component to catalogue

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <div className="footbar">
+      <div>
+        <p>
+          Developed By :
+          <a href="https://github.com/Dipeshtwis"> Dipesh</a>
+        </p>
+      </div>
+      <div><p>Made with Love</p></div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Catalogue = () => (
-  <Header />
+  <>
+    <Header />
+    <Footer />
+  </>
 );
 
 export default Catalogue;
