e39237c046a322fc61de63f19943f7c31b001e64
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 18:08:11 +0530

    Combine all the reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 import recipesFilterReducer from './filter';
 import recipeFetchReducer from './recipe';
+import apiUrlReducer from './apiUrl';
+import categoriesReducer from './categories';
 
 const rootReducer = combineReducers({
-  filter: recipesFilterReducer,
   data: recipeFetchReducer,
+  categories: categoriesReducer,
+  filter: recipesFilterReducer,
+  url: apiUrlReducer,
 });
 
 const store = createStore(rootReducer);
