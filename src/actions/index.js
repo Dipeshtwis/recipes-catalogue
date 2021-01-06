d5afe54766768af472166b9fc7058c8385b0e75e
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 16:26:13 +0530

    Add different fetch action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,21 @@
 export const FILTER = 'FILTER';
+export const RECIPE_FETCH_INIT = 'RECIPE_FETCH_INIT';
+export const RECIPE_FETCH_SUCCESS = 'RECIPE_FETCH_SUCCESS';
+export const RECIPE_FETCH_FAILURE = 'RECIPE_FETCH_FAILURE';
 
 export const filterRecipesAction = category => ({
   type: FILTER,
-  category,
+  payload: category,
+});
+
+export const fetchInitAction = () => ({
+  type: RECIPE_FETCH_INIT,
+});
+
+export const fetchSuccessAction = () => ({
+  type: RECIPE_FETCH_SUCCESS,
+});
+
+export const fetchFailureAction = () => ({
+  type: RECIPE_FETCH_FAILURE,
 });
