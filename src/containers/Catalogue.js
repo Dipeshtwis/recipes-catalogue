c539d850c376779f268e09cec87f99db58e95ff5
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 20:01:53 +0530

    Connect catalogue component to store

--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Filter from '../components/Filter';
 import Footer from '../components/Footer';
 import List from '../components/List';
+import { filterRecipesAction, getCategoriesAction } from '../actions/index';
 
-const Catalogue = () => (
-  <>
-    <Header />
-    <Filter />
-    <List />
-    <Footer />
-  </>
-);
+const Catalogue = props => {
+  const { filterRecipes, getCategories } = props;
 
-export default Catalogue;
+  return (
+    <>
+      <Header />
+      <Filter />
+      <List />
+      <Footer />
+    </>
+  );
+};
+
+const mapStateToProps = state => ({
+  data: state.data,
+  filter: state.filter,
+  url: state.url,
+});
+
+const mapDispatchToProps = dispatch => ({
+  filterRecipes: category => dispatch(filterRecipesAction(category)),
+  getCategories: categories => dispatch(getCategoriesAction(categories)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Catalogue);
