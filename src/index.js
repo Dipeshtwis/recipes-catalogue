045389d58364fd50ceb47c46272ac7132efbbc67
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 15:59:33 +0530

    Use Povider to wrap the App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './reducers/index';
 import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
