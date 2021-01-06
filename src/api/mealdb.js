fc495ab1b5eed1c574320d2ae41346056dc0536c
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 17:25:39 +0530

    Add axios and api url

---
+++ src/api/mealdb.js
@@ -0,0 +1,5 @@
+export const API_ID = 'https://www.themealdb.com/api/json/v1/1/';
+export const API_CATEGORY = 'categories.php';
+export const API_FILTER = 'filter.php?c=';
+export const API_DETAIL = 'lookup.php?i=';
+export const API_SEARCH = 'search.php?s=';
