30b94cfbaa0d0b76bc19983ed6e1730c70405f31
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-07 00:34:30 +0530

    Pass categories to filter as a prop

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Filter = ({ handleFilter }) => (
+const Filter = ({ handleFilter, categories }) => (
   <>
     <h2>Categories</h2>
-    <ul>
-      <li onClick={handleFilter}>Chicken</li>
-      <li>Pizza</li>
-    </ul>
+    <div>
+      {
+        categories.map(category => (
+          <button
+            type="button"
+            key={category.idCategory}
+            onClick={handleFilter}
+            onKeyDown={handleFilter}
+          >
+            {category.strCategory}
+          </button>
+        ))
+      }
+    </div>
   </>
 );
 
 Filter.propTypes = {
   handleFilter: PropTypes.func.isRequired,
+  categories: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
 };
 
 export default Filter;
--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -59,7 +59,7 @@ const Catalogue = props => {
   return (
     <>
       <Header />
-      <Filter handleFilter={handleFilter} />
+      <Filter handleFilter={handleFilter} categories={categories} />
       <List />
       <Footer />
     </>
