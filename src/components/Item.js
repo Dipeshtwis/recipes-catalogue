13c36162c67bc40c99c3cb9fc2be595fd01c525d
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 15:22:01 +0530

    Add item component

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ingredient from '../assets/img/meatballs.jpg';
+
+const Item = () => (
+  <div>
+    <h4>MeatBall</h4>
+    <img src={ingredient} alt="pic" />
+  </div>
+);
+
+export default Item;
