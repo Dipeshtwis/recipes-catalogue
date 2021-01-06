d65503bb4dc3b40ef1bc21981bceee8d2605a19f
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-06 15:04:25 +0530

    Add filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Filter = () => (
+  <>
+    <h2>Categories</h2>
+    <ul>
+      <li>Chicken</li>
+      <li>Pizza</li>
+    </ul>
+  </>
+);
+
+export default Filter;
--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Filter from '../components/Filter';
 import Footer from '../components/Footer';
 
 const Catalogue = () => (
   <>
     <Header />
+    <Filter />
     <Footer />
   </>
 );
