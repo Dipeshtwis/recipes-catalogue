db606724db24a6ad1b6776c0c3ca6a8557c1cf3a
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-07 20:27:03 +0530

    Add route to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
+import { Route } from 'react-router-dom';
 import Catalogue from '../containers/Catalogue';
+import RecipeDetail from '../containers/RecipeDetail';
 import Header from './Header';
 import Footer from './Footer';
 
 const App = () => (
   <>
     <Header />
-    <Catalogue />
+    <Route exact path="/">
+      <Catalogue />
+    </Route>
+    <Route exact path="/:id">
+      <RecipeDetail />
+    </Route>
     <Footer />
   </>
 );
