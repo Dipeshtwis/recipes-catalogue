bf275d81866d9da0fd54fd3f38fce217a183e4dc
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-07 20:37:34 +0530

    Wrap route under browserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './reducers/index';
 import './index.css';
@@ -8,7 +9,9 @@ import App from './components/App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
