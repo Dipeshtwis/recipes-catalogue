fb7ba9bd0fad911851d66c1bc420a7d85f9982d3
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-10 19:12:33 +0530

    Add test for filter reducer

---
+++ src/__tests__/Reducer/filter.test.js
@@ -0,0 +1,16 @@
+import { filterRecipesAction } from '../../actions/index';
+import recipesFilterReducer from '../../reducers/filter';
+
+describe('recipesFilterReducer', () => {
+  it('returns a category to filter when action is "FILTER"', () => {
+    expect(recipesFilterReducer('', filterRecipesAction('Chicken'))).toEqual('Chicken');
+  });
+
+  it('returns the state if the action is not "FILTER"', () => {
+    expect(recipesFilterReducer('state', { type: 'OTHER' })).toEqual('state');
+  });
+
+  it('returns an default props (string) if state is undefined and the action is not "FILTER', () => {
+    expect(recipesFilterReducer(undefined, { type: 'OTHER' })).toEqual('');
+  });
+})
