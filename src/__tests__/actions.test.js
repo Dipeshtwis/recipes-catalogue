482c4c6dc2c1a1e29adc6fd0303ebf8e3f26b5d8
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-10 20:06:31 +0530

    Add test for get categories action

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -2,13 +2,20 @@ import {
   filterRecipesAction,
   getCategoriesAction,
   getRecipeAction,
-} from '../../actions/index';
+} from '../actions/index';
 
 describe('actions', () => {
   describe('filterRecipesAction', () => {
     const payload = 'Chicken';
     it('returns an object with payload and type properties', () => {
       expect(filterRecipesAction(payload)).toEqual({ type: 'FILTER', payload: 'Chicken' });
+    });
+  });
+
+  describe('getCategoriesAction', () => {
+    const payload = ['Chicken'];
+    it('returns an object with payload and type properties', () => {
+      expect(getCategoriesAction(payload)).toEqual({ type: 'CATEGORIES', payload: ['Chicken'] });
     });
   });
 });
